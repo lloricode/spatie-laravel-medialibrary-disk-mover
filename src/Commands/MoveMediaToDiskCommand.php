ff4bb1892e0ebf861ac2fc59745a6909abf95f78
Lloric Mayuga Garcia <lloricode@gmail.com>
2024-06-27 13:15:53 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- src/Commands/MoveMediaToDiskCommand.php
+++ src/Commands/MoveMediaToDiskCommand.php
@@ -6,15 +6,15 @@ namespace Lloricode\SpatieLaravelMediaLibraryDiskMover\Commands;
 
 use Illuminate\Console\Command;
 use Lloricode\SpatieLaravelMediaLibraryDiskMover\Jobs\CollectMediaToMoveJob;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'move-media-to-disk', description: 'Move media from disk to a new disk')]
 class MoveMediaToDiskCommand extends Command
 {
     protected $signature = 'move-media-to-disk {fromDisk} {toDisk}';
 
-    protected $description = 'Move media from disk to a new disk';
-
     /**
-     * @throws \Exception
+     * @throws \Throwable
      */
     public function handle(): int
     {
@@ -30,12 +30,12 @@ class MoveMediaToDiskCommand extends Command
     }
 
     /**
-     * @throws \Exception
+     * @throws \Throwable
      */
     private function checkIfDiskExists(string $diskName): void
     {
         if (! config("filesystems.disks.{$diskName}.driver")) {
-            throw new \Exception("Disk driver for disk `{$diskName}` not set.");
+            $this->fail("Disk driver for disk `{$diskName}` not set.");
         }
     }
 }
